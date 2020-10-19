3a562040241a43ab747e6c73a18aebead4d4948d
sergeykochetkov <sergeykochetkov@yandex.ru>
2020-10-19 10:59:29 +0300

    eda_implemented

--- data.py
+++ data.py
@@ -1,15 +1,43 @@
-
+import torch
+import numpy as np
 
 class TextDataset(object):
     def __init__(self, pos_text_corpus_path, neg_text_corpus_path ):
+        self._tokenizer = torch.hub.load('huggingface/pytorch-transformers', 'tokenizer', 'bert-base-cased')
+        self._load_corpus('_pos_text_corpus',pos_text_corpus_path)
+        self._load_corpus('_neg_text_corpus', neg_text_corpus_path)
 
-        with open(pos_text_corpus_path,'rt') as file:
-            self._pos_text_corpus = file.readlines()
+    def _tokenize(self, sent_list):
 
-        with open(neg_text_corpus_path, 'rt') as file:
-            self._neg_text_corpus = file.readlines()
+        sent_list = [self._tokenizer.encode(sent,add_special_tokens=False) for sent in sent_list]
+        return sent_list
 
-    def
+    def _load_corpus(self, attr_name, path_to_corpus):
+        with open(path_to_corpus, 'rt') as file:
+            sent_list = file.readlines()
+            sent_list = self._tokenize(sent_list)
+            setattr(self, attr_name, sent_list)
+
+    @property
+    def pos_text_corpus(self):
+        return self._pos_text_corpus
+
+    @property
+    def neg_text_corpus(self):
+        return self._neg_text_corpus
+
+    def untokenize(self, token_or_list):
+        if not isinstance(token_or_list, (list,np.ndarray)):
+            tokens_list=[token_or_list]
+        else:
+            tokens_list=token_or_list
+
+        return  self._tokenizer.decode(tokens_list)
+
+
+
+
+
 
 
 
--- explonatory_data_analysis.py
+++ explonatory_data_analysis.py
@@ -1,4 +1,6 @@
 import argparse
+import matplotlib.pyplot as plt
+from collections import Counter
 from config_from_file import config_from_file
 from data import TextDataset
 
@@ -10,4 +12,40 @@ if __name__ == "__main__":
 
     dataset=TextDataset(cfg.pos_corpus_path, cfg.neg_corpus_path)
 
-    dbg=1
+    token_sets=dict()
+
+    for name, tokenized_corpus in zip(['pos', 'neg'], [dataset.pos_text_corpus,dataset.neg_text_corpus]):
+
+        tokens=[]
+        avg_length=0
+        for t in tokenized_corpus:
+            avg_length+=len(t)
+            tokens.extend(t)
+        avg_length/=len(tokenized_corpus)
+        plt.hist(tokens)
+        plt.title(f'hist of {name} tokens')
+        plt.show()
+
+        counter=Counter(tokens)
+        token_sets[name] = (set(tokens), counter)
+        print(f'{name} descriptive stat:')
+        most_common=counter.most_common()
+        print(f'        most frequent words in {name}: ')
+        for token, freq in most_common[:10]:
+            print(f'            {dataset.untokenize(token)} {freq/len(tokens)}')
+
+        print(f'        least frequent words in {name} ')
+        for token, freq in most_common[-10:]:
+            print(f'          {dataset.untokenize(token)} {freq / len(tokens)}')
+
+        print(f'        average sent length {avg_length}')
+
+    name1='pos'
+    name2='neg'
+    for _ in range(2):
+        print(f'{name1} words absend in {name2}')
+        diff_tokens=token_sets[name1][0]-token_sets[name2][0]
+        list_diff_tokens=[(t,token_sets[name1][1].get(t)) for t in diff_tokens]
+        list_diff_tokens.sort(key = lambda x:x[1], reverse=True)
+        print(dataset.untokenize([t[0] for t in list_diff_tokens]))
+        name1,name2=name2,name1
