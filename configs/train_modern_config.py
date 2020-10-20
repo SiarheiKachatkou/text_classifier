fd350f4e22b190600b3a6d40748ed3e034117fc1
sergeykochetkov <sergeykochetkov@yandex.ru>
2020-10-20 10:02:32 +0300

    readme

--- configs/train_modern_config.py
+++ configs/train_modern_config.py
@@ -2,7 +2,7 @@ from .base.base_dataset import *
 from .base.base_train import *
 from .base.base_runtime import *
 
-classifier_head_str = 'BoostingClassifierHead(n_estimators=500, n_jobs=-1)'
+classifier_head_str = 'BoostingClassifierHead(n_estimators=100)'
 feature_extractor_str = 'BERTFeatureExtractor()'
 
 work_dir = 'artifacts/modern'
